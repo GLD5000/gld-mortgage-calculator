fb45d81ee71e438e3274817020f4194562206380
GLD5000 <garethlouisdevlin@gmail.com>
2025-02-01 11:50:41 +0000

    Add src folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_40px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        <h1 className="text-4xl text-center mx-auto">GLD Mortgage Calculator</h1>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 grid gap-2 flex-wrap items-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/GLD5000"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -20,8 +20,9 @@ export default function Home() {
             width={40}
             height={40}
           />
-          My Portfolio
+          Portfolio
         </a>
+        <p className="text-xs text-center">Gareth L Devlin © {`${new Date().getFullYear()}`}</p>
       </footer>
     </div>
   );
