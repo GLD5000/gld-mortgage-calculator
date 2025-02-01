f862d43ef46a0d7744bc0664c2ebaa972f6e7381
GLD5000 <garethlouisdevlin@gmail.com>
2025-02-01 12:37:20 +0000

    Add hook variables to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+
 import Image from "next/image";
+import {
+  useCurrentMortgage,
+  useSalePrice,
+  useAgentRate,
+  useSellingSolicitorFees,
+  useAdditionalCapital,
+  useBuyingSolicitorFees,
+  useHousePrice,
+  useAdditionalLoan,
+  useMortgageTerm,
+  useMortgageRate,
+  useProductFee,
+  useFixedTerm,
+  useRegularOverpayment,
+} from "../hooks/inputHooks";
 
 export default function Home() {
+  const [currentMortgage, setCurrentMortgage] = useCurrentMortgage();
+  const [salePrice, setSalePrice] = useSalePrice();
+  const [agentRate, setAgentRate] = useAgentRate();
+  const [sellingSolicitorFees, setSellingSolicitorFees] =
+    useSellingSolicitorFees();
+  const [additionalCapital, setAdditionalCapital] = useAdditionalCapital();
+  const [buyingSolicitorFees, setBuyingSolicitorFees] =
+    useBuyingSolicitorFees();
+  const [housePrice, setHousePrice] = useHousePrice();
+  const [additionalLoan, setAdditionalLoan] = useAdditionalLoan();
+  const [mortgageTerm, setMortgageTerm] = useMortgageTerm();
+  const [mortgageRate, setMortgageRate] = useMortgageRate();
+  const [productFee, setProductFee] = useProductFee();
+  const [fixedTerm, setFixedTerm] = useFixedTerm();
+  const [regularOverpayment, setRegularOverpayment] = useRegularOverpayment();
   return (
     <div className="grid grid-rows-[20px_1fr_40px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <h1 className="text-4xl text-center mx-auto">GLD Mortgage Calculator</h1>
+        <h1 className="text-4xl text-center mx-auto">
+          GLD Mortgage Calculator
+        </h1>
       </main>
       <footer className="row-start-3 grid gap-2 flex-wrap items-center">
         <a
@@ -22,7 +56,9 @@ export default function Home() {
           />
           Portfolio
         </a>
-        <p className="text-xs text-center">Gareth L Devlin © {`${new Date().getFullYear()}`}</p>
+        <p className="text-xs text-center">
+          Gareth L Devlin © {`${new Date().getFullYear()}`}
+        </p>
       </footer>
     </div>
   );
--- src/hooks/inputHooks.ts
+++ src/hooks/inputHooks.ts
@@ -3,92 +3,92 @@ import { useQueryParams } from "../utils/searchParamsURL";
  * Current mortgage amount in £k
  * @returns {[string,function(string): {void}]}
  */
-export function useCurrentMortgage(){
-    return useQueryParams('cm','202')
+export function useCurrentMortgage() {
+  return useQueryParams("cm", "202");
 }
 /**
  * Sale price in £k
  * @returns {[string,function(string): {void}]}
  */
-export function useSalePrice(){
-    return useQueryParams('sp','415')
+export function useSalePrice() {
+  return useQueryParams("sp", "415");
 }
 /**
  * Selling Agent Rate in %
  * @returns {[string,function(string): {void}]}
  */
-export function useAgentRate(){
-    return useQueryParams('ar','1')
+export function useAgentRate() {
+  return useQueryParams("ar", "1");
 }
 /**
  * Selling Solicitor Fees in £
  * @returns {[string,function(string): {void}]}
  */
-export function useSellingSolicitorFees(){
-    return useQueryParams('ssf','1600')
+export function useSellingSolicitorFees() {
+  return useQueryParams("ssf", "1600");
 }
 /**
  * Additional investment in £k
  * functions as Deposit amount in 'Buying' mode
  * @returns {[string,function(string): {void}]}
  */
-export function useAdditionalCapital(){
-    return useQueryParams('ac')
+export function useAdditionalCapital() {
+  return useQueryParams("ac");
 }
 /**
  * Selling Solicitor Fees in £
  * @returns {[string,function(string): {void}]}
  */
-export function useBuyingSolicitorFees(){
-    return useQueryParams('bsf','2400')
+export function useBuyingSolicitorFees() {
+  return useQueryParams("bsf", "2400");
 }
 /**
  * House Price in £k
  * @returns {[string,function(string): {void}]}
  */
-export function useHousePrice(){
-    return useQueryParams('hp','500')
+export function useHousePrice() {
+  return useQueryParams("hp", "500");
 }
 /**
  * Additional investment in thousands (k)
  * functions as Loan amount in 'Mortgage Only' mode
  * @returns {[string,function(string): {void}]}
  */
-export function useAdditionalLoan(){
-    return useQueryParams('al')
+export function useAdditionalLoan() {
+  return useQueryParams("al");
 }
 /**
  * Mortgage Term in years
  * @returns {[string,function(string): {void}]}
  */
-export function useMortgageTerm(){
-    return useQueryParams('mt','23')
+export function useMortgageTerm() {
+  return useQueryParams("mt", "23");
 }
 /**
  * Mortgage Rate in %
  * @returns {[string,function(string): {void}]}
  */
-export function useMortgageRate(){
-    return useQueryParams('mr','5')
+export function useMortgageRate() {
+  return useQueryParams("mr", "5");
 }
 /**
  * Product Fee in £
  * @returns {[string,function(string): {void}]}
  */
-export function useProductFee(){
-    return useQueryParams('pf','1000')
+export function useProductFee() {
+  return useQueryParams("pf", "1000");
 }
 /**
  * Fixed Term in years
  * @returns {[string,function(string): {void}]}
  */
-export function useFixedTerm(){
-    return useQueryParams('pf','3')
+export function useFixedTerm() {
+  return useQueryParams("pf", "3");
 }
 /**
  * Regular Overpayment in years
  * @returns {[string,function(string): {void}]}
  */
-export function useRegularOverpayment(){
-    return useQueryParams('ro','3')
+export function useRegularOverpayment() {
+  return useQueryParams("ro", "3");
 }
